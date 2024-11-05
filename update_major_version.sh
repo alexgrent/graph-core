284dd4e88f991f8f39e499c39344b1c854075fce
agrent <alexander.michael.grentner@gmail.com>
2024-11-05 14:15:02 +0000

    add versioning script

---
+++ update_major_version.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+current_version=$(mvn -f Graph_core_PIPELINE/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo $current_version 
+major=${current_version%%.*} 
+new_major=$((major + 1)) 
+new_version="${new_major}.0.0"
+echo $new_version 
+mvn versions:set -DnewVersion=$new_versio#!/bin/bash
+
+# Get the current project version using Maven
+current_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Current version: $current_version"
+
+# Extract the major version and increment it
+major=${current_version%%.*}
+new_major=$((major + 1))
+
+# Construct the new version string
+new_version="${new_major}.0.0"
+echo "New version: $new_version"
+
+# Set the new version using Maven
+mvn versions:set -DnewVersion=$new_version
+
+
